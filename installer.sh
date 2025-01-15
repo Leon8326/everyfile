807ba26029c359e69a2e4e1ec3181b5c482c02b7
Leon <132808925+Leon8326@users.noreply.github.com>
2025-01-15 17:03:20 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/sudo bash
+
 echo "Installing the everyfile script in 10 seconds... Please press Ctrl+C to cancel."
 sleep 10
 mkdir /.everyfile
-git clone https://github.com/Leon8326/everyfile /.everyfile/git
-cd /.everyfile/git
+git clone https://github.com/Leon8326/everyfile.git /.everyfile/git
 mkdir /.everyfile/bin
-cp everyfile.sh /.everyfile/bin/everyfile.sh
+cp /.everyfile/git/everyfile.sh /.everyfile/bin/everyfile.sh
 cd /.everyfile
 rm -rf git
-cd bin
-chmod -x everyfile.sh
-mv everyfile.sh everyfile
+chmod -x /.everyfile/bin/everyfile.sh
+mv /.everyfile/bin/everyfile.sh /.everyfile/bin/everyfile
 echo >> ~/.bashrc
 echo "/.everyfile/bin" >> ~/.bashrc
 echo "Installation complete! Now you can use 'sudo everyfile' to search all across your computer"
-exit
